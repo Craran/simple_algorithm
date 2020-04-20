ae7d1358acf29eac1774c584395159e737f15bd9
learning_algorithm <58621928+Craran@users.noreply.github.com>
2020-04-20 17:38:42 +0800

    Create quick_sort.py

---
+++ D&C/quick_sort.py
@@ -0,0 +1,8 @@
+def qucick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return qucick_sort(less) + [pivot] + qucick_sort(greater)
